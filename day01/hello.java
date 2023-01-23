5d61741816c1f9fc836f93ea14bcb2be4bdfe037
HsiaoCz <984274788@qq.com>
2023-01-23 17:04:55 +0800

    anthony

--- day01/hello.java
+++ day01/hello.java
@@ -1,7 +1,7 @@
+package day01;
+
 public class hello {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World!");
     }
 }
-
-// 简单来一个hello World
---
+++ day01/vars.java
@@ -0,0 +1,21 @@
+package day01;
+
+public class vars {
+   public static void main(String[] args) {
+      // 变量
+      // java的编译器将代码转换成字节码
+      // 字节码给jvm执行
+      String a = "Hello Java";
+      System.out.println(a);
+      // 所谓的变量就是可以改变的向量存储
+      int b = 12;
+      System.out.println(b);
+      boolean f = false;
+      System.out.println(f);
+
+      // 声明变量
+      String s;
+      s = "Hi java";
+      System.out.println(s);
+   }
+}
