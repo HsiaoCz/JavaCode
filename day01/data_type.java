04581cfc1d623f69b3dadec083952487b5c47996
HsiaoCz <984274788@qq.com>
2023-04-19 22:05:33 +0800

    java

---
+++ day01/data_type.java
@@ -0,0 +1,13 @@
+package day01;
+
+public class data_type {
+    public static void main(String[] args) {
+        // 变量类型
+        // 1、bit 数据运算的最小单位
+        // 2、字节byte:数据得最小存储单位
+        // 字节和bit可以相互转换
+        // 1byte=8bit
+        // java得数据类型分为：基本数据类型和引用数据类型
+        // 基本数据类型分为数值类型，字符类型，布尔类型
+    }
+}
