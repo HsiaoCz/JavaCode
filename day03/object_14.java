60ab21b699b1c95018b5bcdebbd70c9949c6c3fb
HsiaoCz <984274788@qq.com>
2023-04-24 23:10:10 +0800

    java

---
+++ day03/object_14.java
@@ -0,0 +1,29 @@
+package day03;
+
+public class object_14 {
+    public static void main(String[] args) {
+        // 继承 extends
+        // 如果父类和子类含有相同的属性，可以使用两个关键字进行区分
+        // super和this (super父级属性,this当前属性)
+        // 两个关键字都表示对象，在静态代码块中不能使用
+        Child c = new Child();
+        System.out.println(c.name);
+    }
+}
+
+class Parent {
+    String name = "lis";
+
+    void test() {
+        System.out.println("hello");
+    }
+}
+
+class Child extends Parent {
+    String name = "wangwu";
+
+    void test() {
+        System.out.println(super.name);
+        System.out.println(this.name);
+    }
+}
