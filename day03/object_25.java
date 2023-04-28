56d386f85faa3f2d8c3d291b161f05ccc46f351f
HsiaoCz <984274788@qq.com>
2023-04-28 15:35:00 +0800

    java

---
+++ day03/object_25.java
@@ -0,0 +1,17 @@
+package day03;
+
+public class object_25 {
+    public static void main(String[] args) {
+        // 所谓的访问权限，其实就是访问属性，方法的权力和限制
+        // 谁访问？object_25
+        // 访问谁的？java.lang.Object
+
+        Person25 person25 = new Person25();
+    }
+}
+
+class Person25 {
+   void test() throws Exception{
+      clone()
+   }
+}
---
+++ day03/object_26.java
@@ -0,0 +1,24 @@
+package day03;
+
+public class object_26 {
+    public static void main(String[] args) {
+        // java中不允许外部类使用private修饰
+        // 所谓的外部类，就是在源码中直接声明的类
+        // 所谓的内部类，就是类中声明的类
+
+        // 内部类就当成外部类的属性使用即可
+        // 它需要将外部类创建出来再使用
+        OuterClass outerClass=new OuterClass();
+        OuterClass.InnerClass innerClass=outerClass.new InnerClass();
+    }
+
+    class InnerClass {
+
+    }
+}
+
+class OuterClass {
+    public static class InnerClass {
+
+    }
+}
