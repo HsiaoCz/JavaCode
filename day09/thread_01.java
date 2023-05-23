5bd750966e66513c27a8e1996540b8441c3da1ff
HsiaoCz <984274788@qq.com>
2023-05-24 01:50:30 +0800

    thread

--- day09/thread_01.java
+++ day09/thread_01.java
@@ -3,6 +3,11 @@ package day09;
 public class thread_01 {
     public static void main(String[] args) {
         // java线程
-        
+        // 进程中的一个任务
+        // Thread 线程类
+        // Thread.currentThread().getName();获取当前线程的名称
+        // 当前的线程为main线程
+        // main方法运行在mian线程中
+        Thread.currentThread().getName();
     }
 }
