9ec19f2a3605c3179f8ae40dbe67975175a86228
HsiaoCz <984274788@qq.com>
2023-07-12 15:27:41 +0800

    annotation

---
+++ day20/annotationTest1.java
@@ -0,0 +1,12 @@
+package day20;
+
+public class annotationTest1 {
+
+    // @MyTest3 // 我们这里的注解只能用在类，接口或成员方法上
+    private String name;
+
+    @MyTest3  // 可以用在成员方法上
+    public void test() {
+
+    }
+}
