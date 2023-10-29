80f38b0c6aa07783bb41fe0913c775d88dfd032d
HsiaoCz <984274788@qq.com>
2023-10-29 23:34:36 +0800

    java

---
+++ day25/MyTest222.java
@@ -0,0 +1,7 @@
+package day25;
+
+public @interface MyTest222{
+    String aaa();
+    boolean bbb() default true;
+    String[] ccc();
+}
---
+++ day25/mytest1.java
@@ -0,0 +1,7 @@
+package day25;
+
+@MyTest222(aaa = "hello",ccc = {"hi","lisi"},bbb = true)
+
+public class mytest1 {
+    
+}
