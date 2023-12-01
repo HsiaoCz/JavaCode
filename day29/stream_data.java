d9b335110d4f96ae8e080175b2667008227d49ed
HsiaoCz <984274788@qq.com>
2023-12-01 17:56:11 +0800

    flashback java

---
+++ day29/stream_data.java
@@ -0,0 +1,73 @@
+package day29;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class stream_data {
+    public static void main(String[] args) throws IOException {
+        // DataInputStream
+        // 创建一个 DataInputStream 对象，用于从文件中读取数据
+        DataInputStream dis = new DataInputStream(new FileInputStream("das.txt"));
+
+        // 读取一个字节，将其转换为 byte 类型
+        byte b = dis.readByte();
+        System.out.println(b);
+        // 读取两个字节，将其转换为 short 类型
+        short s = dis.readShort();
+        System.out.println(s);
+        // 读取四个字节，将其转换为 int 类型
+        int i = dis.readInt();
+        System.out.println(i);
+        // 读取八个字节，将其转换为 long 类型
+        long l = dis.readLong();
+        System.out.println(l);
+        // 读取四个字节，将其转换为 float 类型
+        float f = dis.readFloat();
+        System.out.println(f);
+        // 读取八个字节，将其转换为 double 类型
+        double d = dis.readDouble();
+        System.out.println(d);
+        // 读取一个字节，将其转换为 boolean 类型
+        boolean bb = dis.readBoolean();
+        System.out.println(bb);
+        // 读取两个字节，将其转换为 char 类型
+        char ch = dis.readChar();
+        System.out.println(ch);
+        // 关闭 DataInputStream，释放资源
+        dis.close();
+
+        // DataOutputStream
+        // 创建一个 DataOutputStream 对象，用于将数据写入到文件中
+        DataOutputStream das = new DataOutputStream(new FileOutputStream("das.txt"));
+
+        // 将一个 byte 类型的数据写入到文件中
+        das.writeByte(10);
+
+        // 将一个 short 类型的数据写入到文件中
+        das.writeShort(100);
+
+        // 将一个 int 类型的数据写入到文件中
+        das.writeInt(1000);
+
+        // 将一个 long 类型的数据写入到文件中
+        das.writeLong(10000L);
+
+        // 将一个 float 类型的数据写入到文件中
+        das.writeFloat(12.34F);
+
+        // 将一个 double 类型的数据写入到文件中
+        das.writeDouble(12.56);
+
+        // 将一个 boolean 类型的数据写入到文件中
+        das.writeBoolean(true);
+
+        // 将一个 char 类型的数据写入到文件中
+        das.writeChar('A');
+
+        // 关闭 DataOutputStream，释放资源
+        das.close();
+    }
+}
---
+++ day29/stream_file.java
@@ -0,0 +1,89 @@
+package day29;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class stream_file {
+    public static void main(String[] args) throws IOException {
+        // FileInputStream
+        // 声明一个int类型的变量b，用于存储读取到的字节
+        int b;
+        // 创建一个FileInputStream对象，用于读取文件
+        FileInputStream fStream = new FileInputStream("hello.txt");
+
+        // 循环读取文件中的数据
+        while ((b = fStream.read()) != -1) {
+            System.out.println((char) b);
+        }
+
+        // 关闭fileInputStream对象，释放资源
+        fStream.close();
+
+        // FileOutputStream
+        // 创建一个 FileOutputStream 对象，用于写入数据到文件 fos.txt 中
+        FileOutputStream fos = new FileOutputStream("fos.txt");
+
+        // 向文件中写入数据，这里写入的是字符串 "沉默王二" 对应的字节数组
+        fos.write("沉默王二".getBytes());
+
+        // 关闭 FileOutputStream 对象，释放资源
+        fos.close();
+
+        // fileReader
+        // 声明一个 int 类型的变量 b，用于存储读取到的字符
+        int c = 0;
+
+        // 创建一个 FileReader 对象，用于读取文件 read.txt 中的数据
+        FileReader fileReader = new FileReader("read.txt");
+
+        // 循环读取文件中的数据
+        while ((c = fileReader.read()) != -1) {
+            // 将读取到的字符强制转换为 char 类型，并输出到控制台
+            System.out.println((char) c);
+        }
+
+        // 关闭 FileReader 对象，释放资源
+        fileReader.close();
+
+        // FileWriter
+        // 创建一个 FileWriter 对象，用于写入数据到文件 fw.txt 中
+        FileWriter fileWriter = new FileWriter("fw.txt");
+
+        // 将字符串 "沉默王二" 转换为字符数组
+        char[] chars = "沉默王二".toCharArray();
+
+        // 向文件中写入数据，这里写入的是 chars 数组中的所有字符
+        fileWriter.write(chars, 0, chars.length);
+
+        // 关闭 FileWriter 对象，释放资源
+        fileWriter.close();
+
+        // 文件创建
+        File file = new File("test.txt");
+        if (file.createNewFile()) {
+            System.out.println("文件创建成功");
+        } else {
+            System.out.println("文件已经存在");
+        }
+
+        // 删除文件
+        if (file.delete()) {
+            System.out.println("文件删除成功");
+        } else {
+            System.out.println("文件删除失败");
+        }
+
+        // 重命名文件
+        File oldFile = new File("old.txt");
+        File newFile = new File("new.txt");
+        if (oldFile.renameTo(newFile)) {
+            System.out.println("文件重命名成功");
+        } else {
+            System.out.println("文件重命名失败");
+        }
+    }
+}
