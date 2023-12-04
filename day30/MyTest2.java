69ef6ffad9a7d8824e129be9ffbb421177214bac
HsiaoCz <984274788@qq.com>
2023-12-04 19:39:09 +0800

    annotation

---
+++ day30/MyTest2.java
@@ -0,0 +1,5 @@
+package day30;
+
+public @interface MyTest2 {
+    String value(); // 特殊属性
+}
---
+++ day30/at.java
@@ -0,0 +1,10 @@
+package day30;
+
+@MyTest1(aaa = "hello", ccc = { "hi", "h1" }, bbb = true)
+@MyTest2("lisi")
+public class at {
+    @MyTest1(aaa = "张三", ccc = { "hahah", "vaaa" }, bbb = false)
+    public void test1() {
+
+    }
+}
