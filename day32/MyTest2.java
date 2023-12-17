e60396602cfa87d04bf5e80b645dacc826730a84
HsiaoCz <984274788@qq.com>
2023-12-17 08:27:03 +0800

    java

---
+++ day32/MyTest2.java
@@ -0,0 +1,5 @@
+package day32;
+
+public @interface MyTest2 {
+    String value();
+}
---
+++ day32/atest.java
@@ -0,0 +1,10 @@
+package day32;
+
+@MyTest1(aaa = "hello", ccc = { "hi", "ha" }, bbb = true)
+@MyTest2("lisi")
+public class atest {
+    @MyTest1(aaa = "张三", ccc = { "hahah", "vaaa" }, bbb = false)
+    public void test1() {
+
+    }
+}
